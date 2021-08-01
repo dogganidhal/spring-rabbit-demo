148f32eb2f8e15a938577de73331f918e2361a07
Nidhal Dogga <nidhal.dogga@prizy.io>
2021-08-01 22:27:36 +0200

    First commit : create spring boot 2.5.3 and spring cloud 2020.0.3 with rabbitmq binder

---
+++ src/test/java/com/ndogga/demo/DemoSpringApplication.java
@@ -0,0 +1,13 @@
+package com.ndogga.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringApplication {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
